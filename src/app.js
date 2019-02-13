81d9eefafc68ad44cd62542a33d97b9513079d40
chetin4ever <hetan009@gmail.com>
2019-02-13 11:03:46 +0530

    setting up babel with webpack by adding module in webpack.config.js and adding file in root .babelrc

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@
 // console.log(square(13));
 // console.log("app.js is running---!");
 // console.log(canDrink(22));
-import validator from 'validator';
+// import validator from 'validator';
 
-console.log(validator.isEmail('test@gmail.com'));
+// console.log(validator.isEmail('test@gmail.com'));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const templates = <p>hello</p>;
+ReactDOM.render(templates,document.getElementById("app"));
