21ee87d4839904ce74ed7b9e587d1d2c5193fc94
chetin4ever <hetan009@gmail.com>
2019-02-14 14:54:45 +0530

    Adding style to the what should i do button,Add button ,remove button

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,7 +1,10 @@
 import React from 'react';
 const Action = props => (
   <div>
-    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+    <button
+      className="big-button" 
+      onClick={props.handlePick} 
+      disabled={!props.hasOptions}>
       what should i do?
     </button>
   </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,7 +4,11 @@ import Option from './Option';
 const Options = props => (
   <div>
     {props.options.length === 0 && <p>Please add an option to get started...!!!</p>}
-    <button onClick={props.handleDeleteOptions}>RemoveAll</button>
+    <button className="button button--link"
+     onClick={props.handleDeleteOptions}
+    >
+     RemoveAll
+    </button>
     {props.options.map(option => (
       <Option
         key={option}
